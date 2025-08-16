0450bdd512cbf92753efacb03d4f11d731b3c0f5
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-08-16 16:41:23 +0530

    updating favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ import Footer from './components/Footer';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'SOL Staking DApp',
-  description: 'Simple SOL staking application',
+  title: 'Solversity',
+  description: 'Staking and earning nft platform',
 }
 
 export default function RootLayout({
